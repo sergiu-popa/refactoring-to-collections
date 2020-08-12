f3e10b6c01e0273a4268bf77ffb030e1f31e769d
Sergiu Popa <raducu.sergiu@gmail.com>
2020-08-12 14:18:39 +0200

    CVS Surgery 101

---
+++ cvs-surgery-101.php
@@ -0,0 +1,18 @@
+<?php
+
+include('vendor/autoload.php');
+
+$shifts = [
+    'Shipping_Steve_A7',
+    'Sales_B9',
+    'Support_Tara_K11',
+    'J15',
+    'Warehouse_B2',
+    'Shipping_Dave_A6',
+];
+
+$shiftIds = collect($shifts)->map(function ($shift) {
+    return collect(explode('_', $shift))->last();
+});
+
+dd($shiftIds->all());
